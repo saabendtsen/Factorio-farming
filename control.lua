58809feffe95dc9a5d5a19df8a1f3775d0dfdf67
saabendtsen <60225512+saabendtsen@users.noreply.github.com>
2026-08-24 21:06:24 +0200

    Implement contextual field status (#29) (#33)

--- control.lua
+++ control.lua
@@ -30,6 +30,7 @@ remote.add_interface("factorio_farming", {
   debug_pause = slice.debug_pause,
   debug_resume = slice.debug_resume,
   debug_start_next_operation = slice.debug_start_next_operation,
+  contextual_status = slice.contextual_status,
   debug_destroy_tractor = slice.debug_destroy_tractor,
   debug_replace_tractor = slice.debug_replace_tractor,
   snapshot = slice.snapshot,
